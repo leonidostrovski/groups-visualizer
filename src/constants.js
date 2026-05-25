0bfe5c6309ef7c5d67dfdc5f3136864c1eaa4fe0
leonidostrovski <101479051+leonidostrovski@users.noreply.github.com>
2026-05-25 23:53:35 +0300

    v1.2.0

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,11 @@
-export const CARD_VERSION = '1.1.3';
+export const CARD_VERSION = '1.2.0';
 export const NODE_WIDTH = 380;
 export const DAGRE_ESTIMATED_HEIGHT = 300;
 export const AREA_PADDING_X = 32;
 export const AREA_PADDING_TOP = 80;
 export const AREA_PADDING_BOTTOM = 26;
+export const AREA_PAD_X = AREA_PADDING_X;
+export const AREA_PAD_TOP = AREA_PADDING_TOP;
+export const AREA_PAD_BOTTOM = AREA_PADDING_BOTTOM;
 export const AREA_LABEL_FONT_SIZE = 28;
 export const AREA_COUNT_FONT_SIZE = 24;
